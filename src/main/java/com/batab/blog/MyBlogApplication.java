abb5d2b651843292f38316087cca84c988f87738
BaeTab <bhwoo48@gmail.com>
2023-07-03 15:27:30 +0900

    Add Chat API

--- src/main/java/com/batab/blog/MyBlogApplication.java
+++ src/main/java/com/batab/blog/MyBlogApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableWebSocket
 public class MyBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyBlogApplication.class, args);
